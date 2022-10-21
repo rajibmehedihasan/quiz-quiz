9395f4f26b2422fd1efef09b8aab698f3cc4e6a5
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2022-10-21 09:48:36 +0600

    design mini player

--- src/components/quiz/mini-player.tsx
+++ src/components/quiz/mini-player.tsx
@@ -1,5 +1,32 @@
+import videoImg from "../../images/3.jpg";
+
 function MiniPlayer() {
-    return <div>MiniPlayer</div>;
+    return (
+        <div className="miniPlayer floatingBtn">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-play-circle-fill open"
+                viewBox="0 0 16 16"
+            >
+                <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z" />
+            </svg>
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-x-lg close"
+                viewBox="0 0 16 16"
+            >
+                <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+            </svg>
+            <img src={videoImg} alt="Alt" />
+            <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+        </div>
+    );
 }
 
 export default MiniPlayer;
