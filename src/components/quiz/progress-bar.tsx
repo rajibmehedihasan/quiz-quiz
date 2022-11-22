7df412f28e0271b4eac82173c55f1492cd073c62
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2022-11-22 10:02:33 +0600

    add progressbar

--- src/components/quiz/progress-bar.tsx
+++ src/components/quiz/progress-bar.tsx
@@ -1,5 +1,23 @@
 function ProgressBar() {
-    return <div>ProgressBar</div>;
+    return (
+        <div className="progressBar">
+            <div className="backButton">
+                <span className="material-icons-outlined"> arrow_back </span>
+            </div>
+            <div className="rangeArea">
+                <div className="tooltip">24% Cimplete!</div>
+                <div className="rangeBody">
+                    <div className="progress" style={{ width: "20%" }}></div>
+                </div>
+            </div>
+            <a href="result.html">
+                <button className="button next">
+                    <span>Next Question</span>
+                    <span className="material-icons-outlined"> arrow_forward </span>
+                </button>
+            </a>
+        </div>
+    );
 }
 
 export default ProgressBar;
