c6850cf8fe8ebe34f06a1c9f0afe032ac5330af9
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2022-11-23 09:14:53 +0600

    style progressbar

--- src/components/quiz/progress-bar.tsx
+++ src/components/quiz/progress-bar.tsx
@@ -1,13 +1,20 @@
+import arrowLeft from "../../images/icons/arrow-left.svg";
+
 function ProgressBar() {
     return (
-        <div className="progressBar">
-            <div className="backButton">
-                <span className="material-icons-outlined"> arrow_back </span>
+        <div className="w-[calc(100vw-100px) fixed left-0 right-0 bottom-5 my-0 mx-auto grid grid-cols-[50px_1fr_auto] items-center justify-between gap-5 rounded-lg border border-[#e5e5e5] bg-white">
+            <div className="ml-[10px] flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-lg bg-[#47f5901c] hover:bg-[#47f5953a] lg:h-[40px] lg:w-[40px]">
+                <img className="h-6 w-6" src={arrowLeft} alt="arrow left" />
             </div>
-            <div className="rangeArea">
-                <div className="tooltip">24% Cimplete!</div>
-                <div className="rangeBody">
-                    <div className="progress" style={{ width: "20%" }}></div>
+            <div className="relative my-0 mx-auto w-full">
+                <div className="top[-70px] absolute left-[calc(70%-65px)] hidden w-[130px] rounded-lg bg-primary-color py-3 px-4 text-xs font-semibold text-white after:absolute after:bottom-0 after:left-1/2 after:h-[15px] after:w-[15px] after:-translate-x-2/4 after:translate-y-full after:rotate-[45deg] after:rounded after:bg-primary-color">
+                    24% Cimplete!
+                </div>
+                <div className="mx-0 my-auto h-1 w-full rounded-[20px] border-none bg-[#e4e8ee] outline-none">
+                    <div
+                        className="relative h-1 bg-primary-color transition-[width_0.3s_ease]"
+                        style={{ width: "20%" }}
+                    ></div>
                 </div>
             </div>
             <a href="result.html">
