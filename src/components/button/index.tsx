21e5e6121ca492325bfbd2a8467d0ae3335756e0
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2022-11-30 05:52:54 +0600

    finish progressbar

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,33 +1,23 @@
 import React from "react";
 interface Props {
     btnText: string;
+    btnIcon?: string;
     className?: string;
 }
 
 function Button(props: any) {
-    const { btnText, className, ...rest } = props;
+    const { btnText, className, btnIcon, ...rest } = props;
     return (
         <button
-            className={`flex cursor-pointer items-center justify-center gap-[10px] rounded-lg border-0 bg-[#00ff84] py-[0.6rem] px-5 text-[15px] font-semibold uppercase text-primary-color hover:bg-primary-color hover:text-white ${className}`}
+            className={`group flex cursor-pointer items-center justify-center gap-[10px] rounded-lg border-0 bg-[#00ff84] py-[0.6rem] px-5 text-[15px] font-semibold uppercase text-primary-color hover:bg-primary-color hover:text-white ${className}`}
             {...rest}
         >
             {btnText}
+            {btnIcon && (
+                <img className="h-6 w-6 group-hover:invert " src={btnIcon} alt="Button Icon" />
+            )}
         </button>
     );
 }
 
 export default Button;
-
-// background: var(--successGreen);
-// color: var(--fontPrimary);
-// padding: 0.6rem 1.2rem;
-// font-weight: 600;
-// font-size: 15px;
-// text-transform: uppercase;
-// display: flex;
-// justify-content: center;
-// align-items: center;
-// gap: 10px;
-// border-radius: 8px;
-// cursor: pointer;
-// border: 0px;
--- src/components/quiz/progress-bar.tsx
+++ src/components/quiz/progress-bar.tsx
@@ -1,13 +1,15 @@
 import arrowLeft from "../../images/icons/arrow-left.svg";
+import arrowRight from "../../images/icons/arrow-right.svg";
+import Button from "../button";
 
 function ProgressBar() {
     return (
-        <div className="w-[calc(100vw-100px) fixed left-0 right-0 bottom-5 my-0 mx-auto grid grid-cols-[50px_1fr_auto] items-center justify-between gap-5 rounded-lg border border-[#e5e5e5] bg-white">
+        <div className="fixed left-0 right-0 bottom-5 my-0 mx-auto grid h-16 w-[calc(100vw-100px)] grid-cols-[50px_1fr_auto] items-center justify-between gap-5 rounded-lg border border-[#e5e5e5] bg-white">
             <div className="ml-[10px] flex h-[50px] w-[50px] cursor-pointer items-center justify-center rounded-lg bg-[#47f5901c] hover:bg-[#47f5953a] lg:h-[40px] lg:w-[40px]">
                 <img className="h-6 w-6" src={arrowLeft} alt="arrow left" />
             </div>
             <div className="relative my-0 mx-auto w-full">
-                <div className="absolute left-[calc(70%-65px)] top-[-70px] w-[130px] rounded-lg bg-primary-color py-3 px-4 text-xs font-semibold text-white after:absolute after:bottom-0 after:left-1/2 after:h-[15px] after:w-[15px] after:-translate-x-1/2 after:translate-y-1/2 after:rotate-45 after:rounded after:bg-primary-color">
+                <div className="absolute left-[calc(70%-65px)] top-[-70px] hidden w-[130px] rounded-lg bg-primary-color py-3 px-4 text-xs font-semibold text-white after:absolute after:bottom-0 after:left-1/2 after:h-[15px] after:w-[15px] after:-translate-x-1/2 after:translate-y-1/2 after:rotate-45 after:rounded after:bg-primary-color">
                     24% Cimplete!
                 </div>
                 <div className="mx-0 my-auto h-1 w-full rounded-[20px] border-none bg-[#e4e8ee] outline-none">
@@ -17,11 +19,9 @@ function ProgressBar() {
                     ></div>
                 </div>
             </div>
+
             <a href="result.html">
-                <button className="button next">
-                    <span>Next Question</span>
-                    <span className="material-icons-outlined"> arrow_forward </span>
-                </button>
+                <Button className="mr-2.5" btnText="Next Question" btnIcon={arrowRight} />
             </a>
         </div>
     );
