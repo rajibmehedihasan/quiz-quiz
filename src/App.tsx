2ed189f7b9b0e39fad8e6867b128b2be1f289f83
rajibmehedihasan <mehedi.hasan2724@gmail.com>
2023-02-07 01:04:02 +0600

    add context for firebse auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/layout";
 import { Home, Login, Signup, Quiz, Result } from "./components/pages/";
 import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
     return (
         <BrowserRouter>
-            <Layout>
-                <Routes>
-                    <Route path="/*" element={<Home />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/quiz" element={<Quiz />} />
-                    <Route path="/result" element={<Result />} />
-                </Routes>
-            </Layout>
+            <AuthProvider>
+                <Layout>
+                    <Routes>
+                        <Route path="/*" element={<Home />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/signup" element={<Signup />} />
+                        <Route path="/quiz" element={<Quiz />} />
+                        <Route path="/result" element={<Result />} />
+                    </Routes>
+                </Layout>
+            </AuthProvider>
         </BrowserRouter>
     );
 }
